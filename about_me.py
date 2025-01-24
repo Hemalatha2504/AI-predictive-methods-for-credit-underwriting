652e00c4a14055e0fbfa8fe858a6c08a0ede754b
Hemalatha_Sathriyasekar <hemalathasathriyasekar@gmail.com>
2025-01-25 02:17:37 +0530

    Create about_me.py

---
+++ about_me.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from styles import apply_styles
+def show():
+    apply_styles()
+    st.title("👨‍💻 About Me")
+    
+    st.markdown("""
+    ## Hi there! I'm **Hemalatha Sathriyasekar**, a AI & Machine Learning Enthusiast 🚀
+    
+    ### 🌟 Key Areas of Interest:
+    - Artificial Intelligence (AI)
+    - Machine Learning (ML)
+    
+    ### 📬 Let's Connect:
+    - **LinkedIn**: (https://www.linkedin.com/in/hemalathasathriyasekar)
+    - **GitHub**: (https://github.com/Hemalatha2504/AI-predictive-methods-for-credit-underwriting)
+    - **Email**: (mailto:hemalathasathriyasekar@gmail.com)
+
+    Feel free to reach out for collaborations, learning, or discussions on anything AI & ML-related!
+    """)
+
+    st.markdown("___")
+    st.markdown("I'm always excited to connect with like-minded individuals and share knowledge!")
